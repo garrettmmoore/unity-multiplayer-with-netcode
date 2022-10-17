fa2de93e697768a7cda6c96363449818fff0b33b
Garrett Moore <28462557+garrettmmoore@users.noreply.github.com>
2022-10-17 05:02:30 -0700

    Add UI buttons to manage Network connection

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour
 {
+    [SerializeField] private Button serverBtn;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
     // Start is called before the first frame update
-    void Start()
+    
+    private void Awake()
     {
+        serverBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartServer();
+        });
         
+        hostBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartHost();
+        });
+        
+        clientBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartClient();
+        });
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
