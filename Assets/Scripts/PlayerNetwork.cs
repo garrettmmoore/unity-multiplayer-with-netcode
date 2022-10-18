d72cc195fe88727ec275cb76bb70e15d0ae0f21e
Garrett Moore <28462557+garrettmmoore@users.noreply.github.com>
2022-10-18 03:05:56 -0700

    Add script to allow for player movement input for both host and client

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,21 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!IsOwner) return;
+
+        Vector3 moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        const float moveSpeed = 3f;
+        transform.position += moveDir * (moveSpeed * Time.deltaTime);
+    }
+}
