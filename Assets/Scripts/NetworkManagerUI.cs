21583767f66b44b7519a23f10b318fbe655fdddf
Garrett Moore <28462557+garrettmmoore@users.noreply.github.com>
2022-10-21 03:49:16 -0700

    Add a connection approval check before host or client can start

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -7,21 +7,62 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
-    // Start is called before the first frame update
-    
+
     private void Awake()
     {
         serverBtn.onClick.AddListener(() => {
             NetworkManager.Singleton.StartServer();
         });
-        
+
         hostBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.NetworkConfig.ConnectionApproval = true;
+            NetworkManager.Singleton.ConnectionApprovalCallback = ApprovalCheck;
             NetworkManager.Singleton.StartHost();
         });
-        
+
         clientBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.NetworkConfig.ConnectionApproval = true;
+            NetworkManager.Singleton.ConnectionApprovalCallback = ApprovalCheck;
             NetworkManager.Singleton.StartClient();
         });
     }
 
+    private void ApprovalCheck(
+        NetworkManager.ConnectionApprovalRequest request,
+        NetworkManager.ConnectionApprovalResponse response
+    )
+    {
+        // The client identifier to be authenticated
+        var clientId = request.ClientNetworkId;
+
+        // Additional connection data defined by user code
+        var connectionData = request.Payload;
+
+        // Your approval logic determines the following values
+        response.Approved = true;
+        
+        response.CreatePlayerObject = true;
+        
+        // Use a prefab variant for the client with id 1
+        if (clientId == 1)
+        {
+            Debug.Log("Creating player object with prefab hash");
+            // The prefab hash value of the NetworkPrefab, if null the default NetworkManager player prefab is used
+            response.PlayerPrefabHash = 2;
+        }
+        else
+        {
+            Debug.Log("Creating player object with default prefab");
+        }
+
+        // Position to spawn the player object (if null it uses default of Vector3.zero)
+        response.Position = Vector3.zero;
+
+        // Rotation to spawn the player object (if null it uses the default of Quaternion.identity)
+        response.Rotation = Quaternion.identity;
+
+        // If additional approval steps are needed, set this to true until the additional steps are complete
+        // once it transitions from true to false the connection approval response will be processed.
+        response.Pending = false;
+    }
 }
